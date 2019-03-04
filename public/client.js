d25a186bfb57da029398863b251326010492c2df
Lisa Reinhardt <reinhal@gmail.com>
2019-03-03 20:20:29 -0600

    updating files

--- public/client.js
+++ public/client.js
@@ -1,9 +1,15 @@
 let apiBaseUrl = '';
 let apiHeaders = {
-    
+
 }
 
-function openWeatherModal() {
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b9269a56aa1a4f25631ef8fca2ac5132
+
+function handleGetWeather() {
+
+}
+
+function displayWeather() {
 
 }
 
