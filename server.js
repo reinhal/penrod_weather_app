662ee7a537b769138947e7c4f3cf58ec472e4a2d
Lisa Reinhardt <reinhal@gmail.com>
2019-03-07 17:39:39 -0600

    updated endpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const fetch = require("node-fetch");
 const { API_BASE_URL, API_KEY} = require('./config');
 
-app.get('/', (req, res, location) => {
+app.get('/weather', (req, res, location) => {
     console.log(location);
     let API_URL = API_BASE_URL + location + '&units=imperial&APPID=' + API_KEY;
     fetch(API_URL)
